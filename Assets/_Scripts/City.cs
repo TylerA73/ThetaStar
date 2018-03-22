5ddc527d4efde730902c02f1923229b6d7316476
arseneaultt2 <arseneaultt2@mymacewan.ca>
2018-03-21 21:56:16 -0600

    Initial commit:

---
+++ Assets/_Scripts/City.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+
+public class City : MonoBehaviour {
+
+	public TextAsset xml;
+	private string open;
+	private string closed;
+	private string name;
+	private float size;
+	private Vector3 start;
+	private Vector3 end;
+
+
+	// Use this for initialization
+	void Start () {
+		string data = xml.text;
+		Parse(data);
+
+		Debug.Log("Name: " + name);
+		Debug.Log("Size: " + size);
+		Debug.Log("Open: " + open);
+		Debug.Log("Closed: " + closed);
+	}
+	
+	void Parse(string data){
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml(data);
+
+		foreach(XmlElement element in xmlDoc.SelectNodes("City")){
+			name = element.GetAttribute("Name");
+			size = float.Parse(element.GetAttribute("Size"));
+			open = element.GetAttribute("Open");
+			closed = element.GetAttribute("Closed");
+		}
+	}
+}
