0372f09f5f00b5477750af04743fa0ef310d8556
arseneaultt2 <arseneaultt2@mymacewan.ca>
2018-05-18 17:34:58 -0600

    Pushing the project to make room. This project is incomplete.

--- Assets/_Scripts/City.cs
+++ Assets/_Scripts/City.cs
@@ -7,34 +7,71 @@ using System.Xml;
 public class City : MonoBehaviour {
 
 	public TextAsset xml;
-	private string open;
-	private string closed;
+	private char open;
+	private char closed;
 	private string name;
 	private float size;
 	private Vector3 start;
 	private Vector3 end;
+	private List<Node> points;
 
 
 	// Use this for initialization
 	void Start () {
 		string data = xml.text;
-		Parse(data);
-
-		Debug.Log("Name: " + name);
-		Debug.Log("Size: " + size);
-		Debug.Log("Open: " + open);
-		Debug.Log("Closed: " + closed);
+		ParseXml(data);
 	}
 	
-	void Parse(string data){
+	void ParseXml(string data){
 		XmlDocument xmlDoc = new XmlDocument();
 		xmlDoc.LoadXml(data);
 
 		foreach(XmlElement element in xmlDoc.SelectNodes("City")){
 			name = element.GetAttribute("Name");
 			size = float.Parse(element.GetAttribute("Size"));
-			open = element.GetAttribute("Open");
-			closed = element.GetAttribute("Closed");
+			open = char.Parse(element.GetAttribute("Open"));
+			closed = char.Parse(element.GetAttribute("Closed"));
+
+			string start = element.GetAttribute("Start");
+			float sx = float.Parse(start.Split(',')[0]);
+			float sz = float.Parse(start.Split(',')[1]);
+
+			this.start = new Vector3(sx, 0f, sz);
+
+			string end = element.GetAttribute("End");
+			float ex = float.Parse(end.Split(',')[0]);
+			float ez = float.Parse(end.Split(',')[1]);
+
+			this.end = new Vector3(ex, 0f, ez);
+		}
+
+		foreach(XmlElement element in xmlDoc.SelectNodes("City/Line")){
+			string line = element.InnerXml;
+			int z = int.Parse(element.GetAttribute("Id"));
+			ParseLine(line, z);
+		}
+	}
+
+	void ParseLine(string l, int z){
+		char[] line = l.ToCharArray();
+
+		for(int x = 0; x<size; x++){
+			Vector3 pos = new Vector3(x, 0, z);
+			GameObject go;
+			
+			if(line[x] == this.closed){
+				go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				go.transform.position = pos;
+				go.GetComponent<Renderer>().material.color = Color.grey;
+				go.name = "Closed";
+			}else{
+				go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				go.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+				go.transform.position = pos;
+				go.GetComponent<Renderer>().material.color = Color.red;
+				
+				go.name = "Open";
+			}
 		}
 	}
 }
---
+++ Assets/_Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node{
+	private float totalCost{ get; set; }
+	private float cost { get; set; }
+	private Vector3 position;
+	public Node from;
+
+	public Node(Vector3 pos){
+		totalCost = Mathf.Infinity;
+		cost = 1f;
+		position = pos;
+	}
+}
